7b2845a434d41a4c575e5dd6e1f0d7aa856a2e72
merlinis12 <144267044+merlinis12@users.noreply.github.com>
2025-02-14 14:19:15 -0700

    Update and rename requirements.txt to requirements.R

---
+++ requirements.R
@@ -0,0 +1,8 @@
+# Install necessary packages
+install.packages(c("tidyverse", "ggplot2", "ggrepel", "ggnewscale"))
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install(c("DESeq2", "edgeR", "limma", "AnnotationDbi", "org.Hs.eg.db", "apeglm"))
+
+
+if (!requireNamespace("pheatmap", quietly = TRUE)) install.packages("pheatmap")
